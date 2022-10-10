ec63720232451cff8b82cda184f0cf0ae035fa72
Siddharth Kumar Panda <78466559+siddharth-kp@users.noreply.github.com>
2022-10-10 18:29:44 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -2,35 +2,69 @@ class Solution {
 public:
     string breakPalindrome(string palindrome)
     {
-        if(palindrome.length()<=1)  return "";
-        int mini = -1, mini_char='9';
-        map<char,vector<int>> m;
-        for(int i=0;i<palindrome.size();i++)  m[palindrome[i]].push_back(i);
-        if(m.size()==1)
+        // if(palindrome.length()<=1)  return "";
+        // int mini = -1, mini_char='9';
+        // map<char,vector<int>> m;
+        // for(int i=0;i<palindrome.size();i++)  m[palindrome[i]].push_back(i);
+        // if(m.size()==1)
+        // {
+        //     if(palindrome[0]=='a')
+        //         return palindrome.substr(0,1)+'b'+palindrome.substr(2,palindrome.length()-2);
+        //     else
+        //         return 'a'+palindrome.substr(1,palindrome.length()-1);
+        // }
+        // if(palindrome[0]!='a')
+        //     return 'a'+ palindrome.substr(1,palindrome.length()-1);
+        // if(m.size()==2 && palindrome[0]=='a')
+        // {
+        //     if(((*(++m.begin())).second).size()==1)
+        //         return palindrome.substr(0,palindrome.size()-1)+'b';
+        //     else
+        //     {
+        //         int pos2 = ((*(++m.begin())).second)[0]; // first b
+        //         return palindrome.substr(0,pos2)+'a'+palindrome.substr(pos2+1,palindrome.length()-pos2);
+        //     }
+        // }
+        // int pos1 = ((*m.begin()).second)[1]; // second a
+        // int pos2 = ((*(++m.begin())).second)[0]; // first b
+        // cout<<pos1<<" "<<pos2;
+        // if(pos1<pos2)
+        //     return palindrome.substr(0,pos1)+'b'+palindrome.substr(pos1+1,palindrome.length()-pos1);
+        // else
+        //     return palindrome.substr(0,pos2)+'a'+palindrome.substr(pos2+1,palindrome.length()-pos2);
+        int n=palindrome.length();
+        if(n==1)return "";
+        int count =0,na;
+        for(int i=0;i<n;i++)
         {
-            if(palindrome[0]=='a')
-                return palindrome.substr(0,1)+'b'+palindrome.substr(2,palindrome.length()-2);
-            else
-                return 'a'+palindrome.substr(1,palindrome.length()-1);
-        }
-        if(palindrome[0]!='a')
-            return 'a'+ palindrome.substr(1,palindrome.length()-1);
-        if(m.size()==2 && palindrome[0]=='a')
-        {
-            if(((*(++m.begin())).second).size()==1)
-                return palindrome.substr(0,palindrome.size()-1)+'b';
-            else
-            {
-                int pos2 = ((*(++m.begin())).second)[0]; // first b
-                return palindrome.substr(0,pos2)+'a'+palindrome.substr(pos2+1,palindrome.length()-pos2);
+            if(palindrome[i]!='a')
+            {  count++;
+             na=i;
             }
         }
-        int pos1 = ((*m.begin()).second)[1]; // second a
-        int pos2 = ((*(++m.begin())).second)[0]; // first b
-        cout<<pos1<<" "<<pos2;
-        if(pos1<pos2)
-            return palindrome.substr(0,pos1)+'b'+palindrome.substr(pos1+1,palindrome.length()-pos1);
+        if(count>1)
+        {
+            
+        int i=0;
+        for( i=0;i<n;i++)
+        {
+          if(palindrome[i]!='a')
+          {
+              palindrome[i]='a';break;
+          }
+          
+        }
+        if(i==n)
+            palindrome[1]='b';
+        }
+        else if(count==0)
+        {
+            palindrome[n-1]='b';
+        }
         else
-            return palindrome.substr(0,pos2)+'a'+palindrome.substr(pos2+1,palindrome.length()-pos2);
+        {
+            palindrome[n-1]='b';
+        }
+        return palindrome;
     }
 };
