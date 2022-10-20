8d8c2004978c8b8bdcad98f663d242d63d191c36
Siddharth Kumar Panda <78466559+siddharth-kp@users.noreply.github.com>
2022-10-20 11:34:11 +0530

    Time: 51 ms (7.09%), Space: 12.9 MB (7.27%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string intToRoman(int num)
+    {
+        map<int,string> m;
+	    m[1]="I";
+	    m[4]="IV";
+	    m[5]="V";
+	    m[9]="IX";
+	    m[10]="X";
+	    m[40]="XL";
+	    m[50]="L";
+	    m[90]="XC";
+	    m[100]="C";
+	    m[400]="CD";
+	    m[500]="D";
+	    m[900]="CM";
+	    m[1000]="M";
+	    string ans="";
+	    while(num>0)
+        {
+		    for(auto it=m.rbegin();it!=m.rend();it++)
+            {
+			    while(num>=it->first)
+                {
+				    ans+=it->second;
+				    num-=it->first;
+			    }
+		    }
+	    }
+	    return ans;
+    }
+};
