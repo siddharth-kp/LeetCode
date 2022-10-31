f83bd6a159d465cf3cc4c0075c9d2709fdf94b56
Siddharth Kumar Panda <78466559+siddharth-kp@users.noreply.github.com>
2022-10-31 06:42:24 +0530

    Time: 10 ms (98.89%), Space: 17.5 MB (15.16%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -4,27 +4,19 @@ public:
     {
         int n=matrix.size();
         int m=matrix[0].size();
-        for(int i=0;i<m;i++) // 1st row, ith column
+        for(int i=0;i<m;i++)
         {
             int row=0,column=i;
             while(row+1<n && column+1<m)
-            {
-                if(matrix[row][column]!=matrix[row+1][column+1])
+                if(matrix[row][column]!=matrix[++row][++column])
                     return false;
-                row++;
-                column++;
-            }
         }
-        for(int i=0;i<n;i++) // 1st column ith row
+        for(int i=0;i<n;i++)
         {
             int row=i,column=0;
             while(row+1<n && column+1<m)
-            {
-                if(matrix[row][column]!=matrix[row+1][column+1])
+                if(matrix[row][column]!=matrix[++row][++column])
                     return false;
-                row++;
-                column++;
-            }
         }
         return true;
     }
