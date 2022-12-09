07e5efbade7f8b2ff684c691e501336af9283c2a
Siddharth Kumar Panda <78466559+siddharth-kp@users.noreply.github.com>
2022-12-09 11:59:09 +0530

    Time: 50 ms (34.19%), Space: 9 MB (40.04%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,30 +4,48 @@ public:
     {
         if(str.size()==0)   return 0;
         int left=0,right=0;
-        unordered_set<char> s;
+        unordered_map<char,int> m;
+        //m[str[0]]=0;
         int currlen=1,maxlen=1;
         while(right!=str.length())
         {
+            //cout<<left<<" "<<right<<" "<<m.count(str[right])<<endl;
             if(left==right)
             {
-                s.clear();
-                s.insert(str[left]);
+                m.clear();
+                m[str[left]]=left;
                 right++;
             }
-            else if(s.count(str[right]))
+            else if(m.count(str[right]))
             {
-                s.erase(str[left]);
-                left++;
+                for(int i=left;i<=m[str[right]]-1;i++)
+                    m.erase(str[i]);
+                left=m[str[right]]+1;
+                m[str[right]]=right;
+                right++;;
             }
             else
             {
-                s.insert(str[right]);
                 currlen=right-left+1;
                 maxlen=max(currlen,maxlen);
+                m[str[right]]=right;
                 right++;
             }
+            //cout<<left<<" "<<right<<" "<<maxlen<<endl<<endl;
         }
         return maxlen;
     }
         
 };
+
+/*
+
+"abba"
+"abcabcbb"
+"bbbbb"
+"pwwkew"
+"au"
+"cdd"
+"aab"
+
+*/
