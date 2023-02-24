0cbd50a86822530339246a9a563e4b47565e24d6
Siddharth Kumar Panda <78466559+siddharth-kp@users.noreply.github.com>
2023-02-25 02:58:29 +0530

    Time: 620 ms (31.09%), Space: 56.7 MB (89.08%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -3,25 +3,25 @@ public:
     int minimumDeviation(vector<int>& nums)
     {
         priority_queue<int> pq;
-        int minVal = INT_MAX;
-        for(int num : nums)
+        int minval = INT_MAX;
+        for(int num:nums)
         {
-            if (num % 2 == 1)
-                num = num * 2;
+            if(num%2)
+                num*=2;
             pq.push(num);
-            minVal = min(minVal, num);
+            minval=min(minval, num);
         }
         int mini = INT_MAX;
         while(1)
         {
-            int maxVal = pq.top();
+            int maxval = pq.top();
             pq.pop();
-            mini = min(mini, maxVal-minVal);
-            if(maxVal%2==1)
+            mini = min(mini, maxval-minval);
+            if(maxval%2==1)
                 break;
-            maxVal=maxVal/2;
-            minVal=min(minVal, maxVal);
-            pq.push(maxVal);
+            maxval=maxval/2;
+            minval=min(minval, maxval);
+            pq.push(maxval);
         }
         return mini;
     }
