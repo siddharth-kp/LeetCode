59fb23089009c93a1f72fd5aab8c73a1d429d0f9
Siddharth Kumar Panda <78466559+siddharth-kp@users.noreply.github.com>
2023-03-16 09:42:42 +0530

    Time: 35 ms (11.23%), Space: 10.1 MB (98.55%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -10,29 +10,63 @@
  * };
  */
 class Solution {
-public:
+public:    
+    bool levelchange=false;
     
-    int countnodes(TreeNode* root)
+    int fun(TreeNode* root)
     {
         if(root==NULL)
             return 0;
-        return 1+countnodes(root->left)+countnodes(root->right);
+        else
+            return 1+fun(root->left);
     }
     
-    bool checkbt(TreeNode* root, int curr, int n)
+    bool checkbt(TreeNode* root, int currlevel, int &maxlevel)
     {
         if(root==NULL)
             return true;
-        if(curr>n)
+        cout<<"root:"<<root->val<<"   "<<currlevel<<" "<<maxlevel<<"  "<<levelchange<<endl; 
+        if(root->left==NULL && root->right==NULL)
+        {
+            cout<<"leaf\n";
+            if(currlevel==maxlevel)
+                return true;
+            else if(currlevel+1==maxlevel && !levelchange)
+            {
+                maxlevel--;
+                return levelchange=true;
+            }
+            else
+                return false;
+        }
+        if(root->left==NULL && root->right!=NULL)
             return false;
-        return checkbt(root->left,curr*2,n) && checkbt(root->right,curr*2+1,n);
+        bool l=checkbt(root->left,currlevel+1,maxlevel);
+        bool r=checkbt(root->right,currlevel+1,maxlevel);
+        if(root->left!=NULL && root->right==NULL)
+        {
+            if(!levelchange)
+            {
+                levelchange=true;
+                maxlevel--;
+            }
+            else
+                return false;
+        }
+        return l&&r;
     }
     
     bool isCompleteTree(TreeNode* root)
     {
-        if(root==NULL)
-            return true;
-        int n=countnodes(root);
-        return checkbt(root,1,n);
+        int maxlevel=fun(root);
+        return checkbt(root,1,maxlevel);
     }
 };
+
+/*
+[1,2,3,4,5,6]
+[1,2,3,4,5,null,7]
+[1,2,3,5,null,7,8]
+[1,2,3,4,5,6,7,8,9,10,11,12,13,null,null,15]
+[1,2,3,5,6]
+*/
