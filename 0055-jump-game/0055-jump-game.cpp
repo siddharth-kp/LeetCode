70b508eeed4ab52cfbd9448899a5872b19c32119
Siddharth Kumar Panda <78466559+siddharth-kp@users.noreply.github.com>
2023-05-02 12:25:17 +0530

    Time: 956 ms (11.70%), Space: 51.4 MB (7.11%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,50 +1,49 @@
-class Solution {
-public:
-    bool canJump(vector<int>& nums)
-    {
-        if(nums.size()==1)    return 1;
-        vector<bool> dp(nums.size(),0);
-        for(int i=nums.size()-2;i>=0;i--)
-        {
-            int jmp=nums[i];
-            for(int j=i+1;j<=i+jmp && j<nums.size();j++)
-                if(j==nums.size()-1 || dp[j])
-                {
-                    dp[i]=1;
-                    break;
-                }
-        }
-        return dp[0];
-    }
-};
-
-
+//  Tabulation
 
 // class Solution {
 // public:
-    
-//     bool rec(int curr, int end, vector<bool> &dp, vector<int> &nums)
-//     {
-//         // cout<<curr<<endl;
-//         if(curr>=end)
-//             return true;
-//         int jmp=nums[curr];
-//         for(int i=curr+1;i<=curr+jmp && i<nums.size();i++)
-//             if(rec(i,end,dp,nums)==1)
-//             {
-//                 dp[curr]=1;
-//                 dp[i]=1;
-//                 break;
-//             }
-//         return dp[curr];
-//     }
-    
 //     bool canJump(vector<int>& nums)
 //     {
+//         if(nums.size()==1)    return 1;
 //         vector<bool> dp(nums.size(),0);
-//         int a= rec(0,nums.size()-1,dp,nums);
-//         // for(int i:dp)
-//         //     cout<<i<<" ";
-//         return a;
+//         for(int i=nums.size()-2;i>=0;i--)
+//         {
+//             int jmp=nums[i];
+//             for(int j=i+1;j<=i+jmp && j<nums.size();j++)
+//                 if(j==nums.size()-1 || dp[j])
+//                 {
+//                     dp[i]=1;
+//                     break;
+//                 }
+//         }
+//         return dp[0];
 //     }
 // };
+
+
+// Memoization
+
+
+class Solution {
+public:
+    bool recur(int idx, vector<int>& nums, vector<int> &dp)
+    {
+        // cout<<idx<<endl;
+        if(dp[idx]!=-1)
+            return dp[idx];
+        if(idx>nums.size()-1)
+            return 1;
+        int jmp=nums[idx];
+        for(int i=idx+1;i<=idx+jmp && i<nums.size();i++)
+            if(recur(i,nums,dp))
+                return dp[idx]=1;
+        return dp[idx]=0;
+    }
+    
+    bool canJump(vector<int>& nums)
+    {
+        vector<int> dp(nums.size(),-1);
+        dp[nums.size()-1]=1;
+        return recur(0,nums,dp);
+    }
+};
