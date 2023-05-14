2574e79852ea1db8ee5b23d57b8306f0e3a7d04d
Siddharth Kumar Panda <78466559+siddharth-kp@users.noreply.github.com>
2023-05-14 11:12:32 +0530

    Time: 162 ms (9.38%), Space: 17.5 MB (25.32%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minDeletions(string s)
+    {
+        map<int,int> m;
+        for(char ch:s)
+            m[ch-'a']++;
+        vector<int> f;
+        for(auto [i,j]:m)
+            f.push_back(j);
+        sort(f.begin(),f.end());
+        reverse(f.begin(),f.end());
+        m.clear();
+        int curr=f[0];
+        int ans=0;
+        int cons=0;
+        vector<int> f2(f.size());
+        f2[0]=f[0];
+        for(int i=1;i<f.size();i++)
+        {
+            if(f[i]==curr || f[i]>=f2[i-1])
+                f2[i]=f2[i-1]-1;
+            else
+                f2[i]=f[i];
+            curr=f[i];
+        }
+        // for(int i:f)
+        //     cout<<i<<" ";
+        // cout<<endl;
+        // for(int i:f2)
+        //     cout<<i<<" ";
+        // cout<<endl;
+        for(int i=0;i<f.size();i++)
+            ans+=(f2[i]>=0)?(f[i]-f2[i]):f[i];
+        return ans;
+    }
+};
