9e74b3863e5996bb0e9ae1c2df6feb905e27a4cb
Siddharth Kumar Panda <78466559+siddharth-kp@users.noreply.github.com>
2023-05-28 19:12:56 +0530

    Time: 664 ms (5.08%), Space: 69.3 MB (5.77%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    map<pair<int,int>,int> mp;
+    
+    bool recur(vector<int> &piles, int left, int right, int alice, int sum)
+    {
+        // cout<<left<<" "<<right<<endl;
+        if(left>=right)
+            return false;
+        if(left+1==right)
+            return mp[{left,right}]=1;
+        if(mp.count({left,right}))
+            return mp[{left,right}];
+        bool l = recur(piles,left+1,right,1-alice,sum+piles[left]*(alice?1:-1));
+        bool r = recur(piles,left,right-1,1-alice,sum+piles[right]*(alice?1:-1));
+        return mp[{left,right}]=l|r;
+    }
+    
+public:    
+    bool stoneGame(vector<int>& piles)
+    {
+        int alice=1, sum=0;
+        int left=0, right=piles.size()-1;
+        return recur(piles,left,right,1,0);
+    }
+};
