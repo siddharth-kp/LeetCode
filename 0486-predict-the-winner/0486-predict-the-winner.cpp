96678ab79f190a355d2f42be20d6bc1655ae3862
Siddharth Kumar Panda <78466559+siddharth-kp@users.noreply.github.com>
2023-07-28 15:22:38 +0530

    Time: 10 ms (39.29%), Space: 10 MB (5.08%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,13 +1,14 @@
 class Solution {
 private:
     vector<vector<vector<int>>> dp;
+    // int dp[21][21][2];
 
     int recur(vector<int> &nums, int start, int end, int player)
     {
         int ans=player?-1:1;
         if(start>end)
             return 0;
-        cout<<start<<" "<<end<<" "<<player<<" "<<dp[start][end][player]<<endl;
+        // cout<<start<<" "<<end<<" "<<player<<" "<<dp[start][end][player]<<endl;
         if(dp[start][end][player]!=INT_MIN)
             return dp[start][end][player];
         int a = ans*nums[start]+recur(nums,start+1,end,1-player);
@@ -16,16 +17,14 @@ private:
             ans= min(a,b);
         else
             ans= max(a,b);
-        cout<<"dp("<<start<<")("<<end<<")("<<player<<")  : ";
-        cout<<ans<<endl;
+        // cout<<"dp("<<start<<")("<<end<<")("<<player<<")  : ";
+        // cout<<ans<<endl;
         return dp[start][end][player]=ans;
     }
 public:
     bool PredictTheWinner(vector<int>& nums)
     {
-        // memset(dp,-100,sizeof(dp));
-        // if(nums.size()==1)
-        //     return 0;
+        // memset(dp,INT_MIN,sizeof(dp));
         dp.resize(22, vector<vector<int>>(22, vector<int>(2, INT_MIN)));
         return recur(nums,0,nums.size()-1,0)>=0;
     }
