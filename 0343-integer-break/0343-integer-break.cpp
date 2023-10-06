30eccbd0b480fc83ddea2cd83411406e03aced30
Siddharth Kumar Panda <78466559+siddharth-kp@users.noreply.github.com>
2023-10-06 18:46:10 +0530

    Time: 0 ms (100%), Space: 6.8 MB (11.85%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,17 +1,18 @@
 class Solution {
+private:
+    unordered_map<int,int> dp;
 public:
-    
     int integerBreak(int n)
     {
-        if(n<=3)
-            return n-1;
-        int n3=n/3;
-        int n2=n3*3;
-        if(n%3==0)
-            return pow(3,n3);
-        else if(n%3==1)
-            return pow(3,n3-1)*4;
-        else
-            return pow(3,n3)*2;
+        if(n<=1)
+            return 1;
+        if(dp.count(n))
+            return dp[n];
+        int ans=1;
+        for(int i=1;i<n;i++)
+        {
+            ans=max(max(ans,i*(n-i)),i*integerBreak(n-i));
+        }
+        return dp[n]=ans;
     }
 };
