0d052ef44850d5f324a9a27fcfec63844cb9dd8f
Siddharth Kumar Panda <78466559+siddharth-kp@users.noreply.github.com>
2023-12-15 11:13:53 +0530

    Time: 11 ms (50.48%), Space: 11.8 MB (23.80%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string destCity(vector<vector<string>>& paths)
     {
-        map<string,string> mp;
+        unordered_map<string,string> mp;
         for(auto v:paths)
             mp[v[0]]=v[1];
         string str = paths[0][0];
