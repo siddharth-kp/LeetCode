0b7766ea46e2bede88dffce131af45a76ecbced9
Siddharth Kumar Panda <78466559+siddharth-kp@users.noreply.github.com>
2023-12-22 22:55:20 +0530

    Time: 4 ms (23.21%), Space: 6.9 MB (19.64%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxScore(string s)
+    {
+        vector<int> v(s.size(),0);
+        for(int i=0;i<s.size();i++)
+        {
+            if(i==0)
+                v[i]=(s[i]=='0');
+            else
+                v[i]=v[i-1]+(s[i]=='0');
+        }
+        for(int i:v)
+            cout<<i<<" ";
+        cout<<endl;
+        int maxi=0;
+        auto max = [](int a, int b) -> int {
+            if(a>b)
+                return a;
+            else
+                return b;
+        };
+        for(int i=0;i<s.size()-1;i++)
+            maxi=max(maxi,v[i]+s.size()-i-1-v.back()+v[i]);
+        return maxi;
+    }
+};
