cfcd6265cb841b434715003949ab752b3f2c351e
Siddharth Kumar Panda <78466559+siddharth-kp@users.noreply.github.com>
2023-12-26 11:50:44 +0530

    Time: 23 ms (69.34%), Space: 10.3 MB (24.46%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,28 +1,29 @@
 class Solution {
-public:
-    long long MOD = 1e9+7;
-    
-    long long solve(int dice, int faces, int target, vector<vector<int>> &dp)
+private:
+    int mod=1e9+7;
+    vector<vector<int>> dp;
+
+    int recur(int target, int n, int k)
     {
-        if(target < 0)
-            return 0;
-        if(dice == 0 and target != 0)
-            return 0;
-        if(target == 0 and dice != 0)
-            return 0;
-        if(target == 0 and dice == 0)
+        // cout<<target<<" "<<n<<" "<<k<<endl;
+        if(n==0 & target==0)
             return 1;
-        if(dp[dice][target] != -1)
-            return dp[dice][target];
-        long long ans = 0;
-        for(int i=1; i<=faces; i++)
-            ans += solve(dice-1, faces, target-i, dp);
-        return dp[dice][target] = ans%MOD;
+        if(n==0 || target<=0)
+            return 0;
+        if(dp[target][n]!=-1)
+            return dp[target][n];
+        int ans=0;
+        for(int i=1;i<=k;i++)
+        {
+            ans+=recur(target-i,n-1,k);
+            ans%=mod;
+        }
+        return dp[target][n]=ans;
     }
-    
+public:
     int numRollsToTarget(int n, int k, int target)
     {
-        vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
-        return solve(n, k, target, dp);
+        dp.resize(target+1,vector<int> (n+1,-1));
+        return recur(target,n,k)%mod;
     }
 };
