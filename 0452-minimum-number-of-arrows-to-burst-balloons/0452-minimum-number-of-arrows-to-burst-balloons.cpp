66b4653633d639f0bb48f5ec713741ae26796120
Siddharth Kumar Panda <78466559+siddharth-kp@users.noreply.github.com>
2024-03-19 00:05:23 +0530

    Time: 258 ms (55.66%), Space: 99.9 MB (29.39%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,12 +1,11 @@
-bool sort2( const pair<int,int> a,const pair<int,int> b)
+bool sort2(const pair<int,int> &a, const pair<int,int> &b)
 {
     return (a.second < b.second);
 }
     
 class Solution 
 {
-    public:
-    
+    public:    
     int findMinArrowShots(vector<vector<int>>& points) 
     {
         if(points.size()==0)
@@ -17,7 +16,7 @@ class Solution
         for(int i=0;i<points.size();i++)
             v.push_back({points[i][0],points[i][1]});
         sort(v.begin(),v.end(),sort2);
-        long long int a=1,prev = v[0].second;
+        long long int a=1, prev = v[0].second;
         for(int i=1;i<v.size();i++)
         {
             if(v[i].first>prev)
